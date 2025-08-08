6828e31ddb35869d2d470767c2d9707f25ea8a47
Facundo Herrera <facundo.herrera@mi.unc.edu.ar>
2025-08-08 10:07:12 -0300

    CargaActividad 1

--- actividad.py
+++ actividad.py
@@ -1,7 +1,7 @@
 # Historia 2
-# La serie sigue la historia de Johnny Lawrence, exestudiante del dojo Cobra Kai,
+# La serie sigue la historia de [Johnny] [Lawrence], exestudiante del dojo [Cobra Kai],
 # quien busca la redención reabriendo el infame dojo y reavivando, en el proceso,
-# su rivalidad con un ahora exitoso Daniel LaRusso.
+# su rivalidad con un ahora exitoso [Daniel] [LaRusso].
 # Este enemigo ha estado luchando por mantener el equilibrio en su vida sin la guía de su mentor, el señor Miyagi.
 # La serie trata sobre dos hombres que enfrentan sus demonios del pasado y
 # resuelven las frustraciones del presente de la única manera en que saben hacerlo: a través del karate.
@@ -12,6 +12,8 @@ dojo = str(input("Dame el nombre del dojo "))
 nombreHeroe = str(input("Dame el nombre del heroe "))
 apellidoHeroe = str(input("Dame el apellido del heroe "))
 
+print("Este es el nombre de la historia")
 print("La serie sigue la historia de"+nombrePrincipal+" "+apellidoPrincipal+"exestudiante del dojo Cobra Kai,")
 print("quien busca la redención reabriendo el infame dojo y reavivando, en el proceso,")
-print(f"su rivalidad con un ahora exitoso {nombreHeroe} {apellidoHeroe}")
+print("su rivalidad con un ahora exitoso "+nombreHeroe+" "+apellidoHeroe)
+print("Este enemigo ha estado luchando por mantener el equilibrio en su vida sin la guía de su mentor, el señor Miyagi. La serie trata sobre dos hombres que enfrentan sus demonios del pasado y resuelven las frustraciones del presente de la única manera en que saben hacerlo: a través del karate.")
